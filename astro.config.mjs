e5b09d8d4c37b2574be2b73bf6bcf71d87890348
Michael Allen Smith <digitalcolony@gmail.com>
2024-01-13 08:48:21 -0800

    4.1, meta, url fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,19 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import metaTags from "astro-meta-tags";
+
+// https://astro.build/config
 export default defineConfig({
-	site: "https://neilrogers.org",
-	integrations: [
-		react(),
-		sitemap({
-			changefreq: "weekly",
-			priority: 0.7,
-			lastmod: new Date(),
-		}),
-	],
+  site: "https://neilrogers.org",
+  integrations: [react(), sitemap({
+    changefreq: "weekly",
+    priority: 0.7,
+    lastmod: new Date()
+  }), metaTags()]
 });
