b7edbb333b806340e691a3798c8d474fae5570cb
Michael Allen Smith <digitalcolony@gmail.com>
2026-03-13 14:14:33 -0700

    astro 6 updates, new drop

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,13 +1,14 @@
 import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
 const stories = defineCollection({
+	loader: glob({ pattern: "**/*.md", base: "./src/content/stories" }),
 	schema: z.object({
 		author: z.string(),
 		date: z.string(),
 		description: z.string(),
 		title: z.string(),
 	}),
-	type: "content",
 });
 
 export const collections = { stories };
